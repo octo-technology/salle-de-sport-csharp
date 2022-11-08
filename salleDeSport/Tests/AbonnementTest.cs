2f088335f00e7b2db41d35a4888b3c00ae1c69ae
dlamande <dlamande@octo.com>
2022-11-08 09:22:18 +0100

    wip

--- salleDeSport/Tests/AbonnementTest.cs
+++ salleDeSport/Tests/AbonnementTest.cs
@@ -7,10 +7,16 @@ public class AbonnementTest
     public void DoitSouscrireUnAbonnementEtEnvoyerUnMail()
     {
         // GIVEN
-        AbonnementId abonnementId = new AbonnementId("abonnementId");
+        Boolean estEtudiant = false;
+        String formuleId = "aaa-bbb";
+        String email = "test@test.com";
+        String dateDeDebutDAbonnement = "2023-02-02";
+        AbonnementRepository abonnementRepository = new AbonnementRepository();
+        SouscrireAbonnementCommandHandler souscrireAbonnementCommandHandler = new SouscrireAbonnementCommandHandler(abonnementRepository)
+        souscrireAbonnementCommandHandler.handle(formuleId, email, dateDeDebutDAbonnement, estEtudiant);
 
         // WHEN
-        Abonnement abonnementSouscrit = Abonnement.Souscrire("","", "", "", true, new RandomUUIDGeneratorForTest());
+        Abonnement abonnementSouscrit = Abonnement.Souscrire();
 
         // THEN
         Assert.AreEqual(abonnementSouscrit.AbonnementId.Value, "id");
@@ -18,8 +24,16 @@ public class AbonnementTest
     }
 }
 
-class RandomUUIDGeneratorForTest : IRandomUUIDGenerator {
-    string IRandomUUIDGenerator.generateId() {
-        return "id";
+class AbonnementRepository {
+
+}
+
+class SouscrireAbonnementCommandHandler {
+    public SouscrireAbonnementCommandHandler(AbonnementRepository abonnementRepository) {
+
+    }
+
+    public void handle(String formuleId, String email, String dateDeDebutDAbonnement, Boolean estEtudiant) {
+
     }
 }
